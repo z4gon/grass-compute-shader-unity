76a28daa99a0eedcfa8da429d535901039aa23c3
Gonzalo <4588601+z4gon@users.noreply.github.com>
2022-10-28 04:02:41 -0300

    wip procedural mesh

--- Assets/Scripts/Grass.cs
+++ Assets/Scripts/Grass.cs
@@ -1,18 +1,78 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class Grass : MonoBehaviour
+public static class Grass
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    const float Height = 0.2f;
+    const int StepsCount = 4;
+    const float StepHeight = Height / StepsCount;
+    const float HalfWidth = Height / 10;
 
-    // Update is called once per frame
-    void Update()
+    // Start is called before the first frame update
+    public static Mesh CreateGrassMesh()
     {
-        
+        var mesh = new Mesh();
+
+        // define the vertices
+        mesh.vertices = new Vector3[] {
+            // step 0
+            new Vector3(-HalfWidth, 0, 0),
+            new Vector3(HalfWidth, 0, 0),
+
+            // step 1
+            new Vector3(-HalfWidth * 0.9f, StepHeight, 0),
+            new Vector3(HalfWidth * 0.9f, StepHeight, 0),
+
+            // step 2
+            new Vector3(-HalfWidth * 0.8f, 2 * StepHeight, 0),
+            new Vector3(HalfWidth * 0.8f, 2 * StepHeight, 0),
+
+            // step 3
+            new Vector3(-HalfWidth * 0.6f, 3 * StepHeight, 0),
+            new Vector3(HalfWidth * 0.6f, 3 * StepHeight, 0),
+
+            // step 4
+            new Vector3(0, 4 * StepHeight, 0)
+        };
+
+        // define the normals
+        Vector3[] normalsArray = new Vector3[mesh.vertices.Length];
+        Array.Fill(normalsArray, new Vector3(0, 0, 1));
+        mesh.normals = normalsArray;
+
+        mesh.uv = new Vector2[] {
+            // step 0
+            new Vector2(0, 0),
+            new Vector2(1, 0),
+
+            // step 1
+            new Vector2(0, 0.25f),
+            new Vector2(1, 0.25f),
+
+            // step 2
+            new Vector2(0, 0.5f),
+            new Vector2(1, 0.5f),
+
+            // step 3
+            new Vector2(0, 0.75f),
+            new Vector2(1, 0.75f),
+
+            // step 4
+            new Vector2(0.5f, 1f),
+        };
+
+        mesh.SetIndices(
+            // counter clock wise so the normals make sense
+            indices: new int[]{
+                // step 0
+                0,1,2,
+                2,1,3,
+
+            },
+            topology: MeshTopology.Triangles,
+            submesh: 0
+        )
+
+        return mesh;
     }
 }
--- Assets/Scripts/GrassBlade.cs
+++ Assets/Scripts/GrassBlade.cs
@@ -1,18 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class GrassBlade : MonoBehaviour
+struct GrassBlade
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    Vector3 position;
 }
